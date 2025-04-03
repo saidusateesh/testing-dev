3c7294cd331f0a30da023c11b2d27e8e426064d0
kalyanpothabathula <103244648+kalyanpothabathula@users.noreply.github.com>
2025-04-03 14:23:37 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -3,6 +3,6 @@ terraform {
     storage_account_name = "kalyan1234"
     container_name = "terraform"
     key = "terraform.tfstate"
-    access_key = "Db9RCOekoHg+xuKE0K8A9wJSj+ZumvktlyiQS5kYwcXzhKQDbLQdaFyVSTBwhyU29CkKU3jwYoF++AStemsePw=="
+    access_key = "AoTI2G5oRm1FMpQWiiRb1gep2RAacrEHauJxRzidRqCH1nfYzv0MT124LLMnPRmV3YmZBZ/BXVIx+ASt6RvRzg=="
   }
 }
